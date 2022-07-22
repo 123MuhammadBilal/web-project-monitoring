4d0d7670866e1531712c139c4be59a670da71280
123MuhammadBilal <muhammadbilalll2468@gmail.com>
2022-07-22 16:22:29 -0700

    update

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
   <section id="home" class="container-fluid">
     <div class="container">
       <div class="row ">
-        <div class="home-left col-lg-6 col-md-12 col-sm-12">
+        <div class="home-left col-lg-6 col-md-6 col-sm-6">
           <h1>Website</h1>
           <h2>Project Mentoring</h2>
           <p>Dengan Website Project Mentoring Anda Dapat Melihat Data Real Dari Project Yang Sedang Dikerjakan Oleh Tim Kami. Lihat Data lebih lebih lengkap dengan Kilik Tombol dibawah ini.</p>
-          <button class="btn btn-primary"> Lihat Projcet</button>
+          <a href="project.php"><button class="btn btn-primary"> Lihat Projcet</button></a>
         </div>
-        <div class="col home-right col-lg-6 col-md-12 col-sm-12">
+        <div class="col home-right col-lg-6 col-md-6 col-sm-12">
           <img src="img/home-right.png" alt="">
         </div>
       </div>
