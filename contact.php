ee1b05a3439dcff051e937cf946009587c86935a
123MuhammadBilal <muhammadbilalll2468@gmail.com>
2022-07-23 06:51:13 -0700

    update terbaru branch master interview3

--- contact.php
+++ contact.php
@@ -17,7 +17,7 @@
             </div>
             <div class="contact-right col-lg-6 col-md-6 col-sm-12">
                 <h1>Get In Touch</h1>
-                <p>Untuk mengehtahui lebih detail terkait projcet mentoring dan silahkan hubungi kontak kami dibawah ini.</p>
+                <p class="text-center">Untuk mengehtahui lebih detail terkait projcet mentoring dan silahkan hubungi kontak kami dibawah ini.</p>
 
                 <img src="img/bilal1.jpg" width="120" height="120" alt=""> <br>
 
