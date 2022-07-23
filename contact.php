8d1da0c21827a297404c86ec48a00ec0166d7a8a
123MuhammadBilal <muhammadbilalll2468@gmail.com>
2022-07-22 19:37:26 -0700

    update

---
+++ contact.php
@@ -0,0 +1,35 @@
+<?php include 'includes/header.php'; ?>
+
+<section id="contact">
+    <div class="container">
+        <div class="row pt-5 pe-3 ps-3 ">
+            <div class="contact-left col-lg-6 col-md-6 col-sm-12">
+                <h1>Contact Us</h1>
+                <label for="name">Name : </label>
+                <input type="text" class="form-control" name="name" id="name">
+                <label for="email">Email : </label>
+                <input type="text" class="form-control" name="email" id="email">
+                <label for="message">Message : </label>
+                <input type="text" class="form-control" name="message" id="message">
+                <button type="submit" class="btn btn-primary form-control mt-5" name="">Kirim</button>
+            </div>
+            <div class="contact-right col-lg-6 col-md-6 col-sm-12">
+                <h1>Get In Touch</h1>
+
+                <p>Untuk Mengehtahui lebih detail terkait projcet mentoring dan apa saja yang ingin ditananya, silahkan hubngin kontak kami dibawah ini.</p>
+
+                <img src="img/bilal1.jpg" width="150" height="150" alt=""> <br>
+
+                <a href="#"> <i class="fab fa-facebook-f"></i> Bilal </a>
+                <a href="#"> <i class="fab fa-youtube"></i> Muhammad Bilal </a>
+                <a href="#"> <i class="fab fa-instagram"></i> bilals_00 </a>
+
+
+
+
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include 'includes/footer.php'; ?>
--- includes/header.php
+++ includes/header.php
@@ -45,7 +45,7 @@
                             <a class="nav-link" href="project.php">Project</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Contact</a>
+                            <a class="nav-link" href="contact.php">Contact</a>
                         </li>
                     </div>
                 </div>
